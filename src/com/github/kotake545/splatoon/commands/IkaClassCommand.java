34df3e557846189eb94b751fc62649fb41c59279
himanoa <hakkoyozyo@gmail.com>
2015-07-14 02:40:16 +0900

    'refactaring IkaManager'

--- src/com/github/kotake545/splatoon/commands/IkaClassCommand.java
+++ src/com/github/kotake545/splatoon/commands/IkaClassCommand.java
@@ -12,62 +12,93 @@ import com.github.kotake545.splatoon.IkaClass;
 import com.github.kotake545.splatoon.Splatoon;
 
 public class IkaClassCommand extends IkaCommandExecuter {
+	
+	private List<String> classList;
+	private StringBuffer rollClass() {
+		StringBuffer buffer = new StringBuffer();
+		for(int i = 0;i < this.classList.size();i++) {
+			buffer.append(this.classList.get(i));
+			if(!(i==this.classList.size())) {
+				buffer.append(" | ");
+			}
+		}
+		return buffer;
+	}
+	private void showClassListMessage(CommandSender sender,StringBuffer ClassList) {
+		if(classList.isEmpty()){
+			sender.sendMessage(Splatoon.format+"繧ｯ繝ｩ繧ｹ縺檎匳骭ｲ縺輔ｌ縺ｦ縺�縺ｾ縺帙ｓ縲�");
+		}else{
+			sender.sendMessage(Splatoon.format+"Class:"+classList.size());
+			sender.sendMessage(ClassList.toString());
+		}
+	}
+	private void setEquipment(CommandSender sender,Player id,String className) {
+		if(Splatoon.ikaClassManager.getClass(className)!=null){
+			if(id!=null&&id.isOnline()){
+				Splatoon.ikaManager.getIka(id).setClass(Splatoon.ikaClassManager.getClass(className));
+				sender.sendMessage(Splatoon.format+id.getName()+"縺ｫ"+className+"繧定｣�蛯吶＆縺帙∪縺励◆縲�");
+			}else{
+				sender.sendMessage(Splatoon.format+id.getName()+"縺ｨ縺�縺�繝励Ξ繧､繝､繝ｼ縺悟ｭ伜惠縺励∪縺帙ｓ縺ｧ縺励◆縲�");
+			}
+		}else{
+			sender.sendMessage(Splatoon.format+className+" 繧ｯ繝ｩ繧ｹ縺檎匳骭ｲ縺輔ｌ縺ｦ縺�縺ｾ縺帙ｓ縲�");
+		}
+	}
+	private void setClass(CommandSender sender,String className) {
+		if(Splatoon.ikaClassManager.getClass(className)!=null){
+			Splatoon.ikaManager.getIka((Player) sender).setClass(Splatoon.ikaClassManager.getClass(className));
+			sender.sendMessage(Splatoon.format+className+" 繧ｯ繝ｩ繧ｹ繧定｣�蛯吶＠縺ｾ縺励◆縲�");
+		}else{
+			sender.sendMessage(Splatoon.format+className+" 繧ｯ繝ｩ繧ｹ縺檎匳骭ｲ縺輔ｌ縺ｦ縺�縺ｾ縺帙ｓ縲�");
+		}
+	}
+	private Boolean showClassList(final String args[]) {
+		if(args.length==1&&args[0].toLowerCase().equals("classList")){
+			return true;
+		}
+		return false;
+	}
+	private Boolean setEquiment(final String args[]){
+		if(3<=args.length&&args[1].toLowerCase().equals("set")){
+			return true;
+		}
+		return false;
+	}
+	private Boolean setClass(final String args[],CommandSender sender) {
+		if(2<=args.length&&sender instanceof Player){
+			return true;
+		}
+		return false;
+	}
+	public IkaClassCommand()
+	{
+		super();
+		this.classList = new ArrayList<String>();
+	}
+	
 	@SuppressWarnings({ "deprecation" })
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String paramString, String[] args) {
-		if(args.length==1&&args[0].toLowerCase().equals("list")){
-			List<String> list=new ArrayList<String>();
+		if(showClassList(args)){
 			for(IkaClass ikaclass:Splatoon.ikaClassManager.getAllClass()){
-				list.add(ikaclass.className);
+				classList.add(ikaclass.className);
 			}
-			StringBuffer a = new StringBuffer();
-			for (int i = 0; i < list.size();i++) {
-				String name = list.get(i);
-				a.append(name);
-				if(i+1 < list.size()){
-					a.append(" | ");
-				}
-			}
-			if(list.size()<=0){
-				sender.sendMessage(Splatoon.format+"クラスが登録されていません。");
-			}else{
-				sender.sendMessage(Splatoon.format+"Class:"+list.size());
-				sender.sendMessage(a.toString());
-			}
+			showClassListMessage(sender,rollClass());
 			return true;
 		}
-		if(3<=args.length&&args[1].toLowerCase().equals("set")){
-			String className = args[0];
-			Player give=Bukkit.getPlayer(args[2]);
-			if(Splatoon.ikaClassManager.getClass(className)!=null){
-				if(give!=null&&give.isOnline()){
-					Splatoon.ikaManager.getIka(give).setClass(Splatoon.ikaClassManager.getClass(className));
-					sender.sendMessage(Splatoon.format+args[2]+"に"+className+"を装備させました。");
-				}else{
-					sender.sendMessage(Splatoon.format+args[2]+"というプレイヤーが存在しませんでした。");
-				}
-			}else{
-				sender.sendMessage(Splatoon.format+className+" クラスが登録されていません。");
-			}
+		else if(setEquiment(args)){
+			//プレイヤーに職業に応じた装備を装着させる場合
+			setEquipment(sender,Bukkit.getPlayer(args[2]),args[0]);
 			return true;
 		}
-		if(2<=args.length&&sender instanceof Player){
-			String className = args[0];
-			if(args[1].toLowerCase().equals("set")){
-				if(Splatoon.ikaClassManager.getClass(className)!=null){
-					Splatoon.ikaManager.getIka((Player) sender).setClass(Splatoon.ikaClassManager.getClass(className));
-					sender.sendMessage(Splatoon.format+className+" クラスを装備しました。");
-				}else{
-					sender.sendMessage(Splatoon.format+className+" クラスが登録されていません。");
-				}
-				return true;
-			}
+		else if(setClass(args,sender)){
+			setClass(sender,args[0]);
 		}
 		return unknown(sender, paramString);
 	}
 
 	private Boolean unknown(CommandSender sender,String paramString){
-		sender.sendMessage(Splatoon.format+"/"+paramString+" list");
+		sender.sendMessage(Splatoon.format+"/"+paramString+" classList");
 		sender.sendMessage(Splatoon.format+"/"+paramString+" [classname] set");
 		sender.sendMessage(Splatoon.format+"/"+paramString+" [classname] set [playername]");
 		return true;
